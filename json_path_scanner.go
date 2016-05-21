e85444c0e2b857caa2175d10412575d3deccc874
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-05-21 18:32:08 +0900

    initial import

---
+++ json_path_scanner.go
@@ -0,0 +1,45 @@
+package json_path_scanner
+
+import (
+	"strconv"
+	"strings"
+)
+
+type PathValue struct {
+	Path  string
+	Value interface{}
+}
+
+func newPathValue(key string, value interface{}) PathValue {
+	return PathValue{
+		Path:  key,
+		Value: value,
+	}
+}
+
+func Scan(value interface{}, ch chan<- PathValue) {
+	defer close(ch)
+	scanJson("$", value, ch)
+}
+
+func scanJson(label string, value interface{}, ch chan<- PathValue) {
+	switch value.(type) {
+	case int, float64, string, nil:
+		ch <- newPathValue(label, value)
+	case map[string]interface{}:
+		m := value.(map[string]interface{})
+		for k, v := range m {
+			if strings.Contains(k, ".") {
+				scanJson(label+"['"+k+"']", v, ch)
+			} else {
+				scanJson(label+"."+k, v, ch)
+			}
+		}
+	case []interface{}:
+		for i, v := range value.([]interface{}) {
+			scanJson(label+"["+strconv.Itoa(i)+"]", v, ch)
+		}
+	default:
+		panic("Unsupported type in json")
+	}
+}
---
+++ json_path_scanner_test.go
@@ -0,0 +1,95 @@
+package json_path_scanner
+
+import (
+	"log"
+	"testing"
+)
+
+func TestInt(t *testing.T) {
+	ch := make(chan PathValue)
+	go func() {
+		Scan(3, ch)
+	}()
+
+	p := <-ch
+	log.Print(p)
+	if p.Path != "$" {
+		t.Fatalf("Path should be $ but %s", p.Path)
+	}
+	if p.Value != 3 {
+		t.Fatalf("Value should be 3 but %s", p.Value)
+	}
+}
+
+func TestFloat(t *testing.T) {
+	ch := make(chan PathValue)
+	go func() {
+		Scan(3.14, ch)
+	}()
+
+	p := <-ch
+	log.Print(p)
+	if p.Path != "$" {
+		t.Fatalf("Path should be $ but %s", p.Path)
+	}
+	if p.Value != 3.14 {
+		t.Fatalf("Value should be 3.14 but %s", p.Value)
+	}
+}
+
+func TestString(t *testing.T) {
+	ch := make(chan PathValue)
+	go func() {
+		Scan("Foo", ch)
+	}()
+
+	p := <-ch
+	log.Print(p)
+	if p.Path != "$" {
+		t.Fatalf("Path should be $ but %s", p.Path)
+	}
+	if p.Value != "Foo" {
+		t.Fatalf("Value should be 'Foo' but %s", p.Value)
+	}
+}
+
+func TestArray(t *testing.T) {
+	ch := make(chan PathValue)
+	go func() {
+		Scan([]interface{}{5963, 4649}, ch)
+	}()
+
+	p := <-ch
+	log.Print(p)
+	if p.Path != "$[0]" {
+		t.Fatalf("Path should be $[0] but %s", p.Path)
+	}
+	if p.Value != 5963 {
+		t.Fatalf("Value should be 5963 but %s", p.Value)
+	}
+
+	p = <-ch
+	if p.Path != "$[1]" {
+		t.Fatalf("Path should be $[1] but %s", p.Path)
+	}
+	if p.Value != 4649 {
+		t.Fatalf("Value should be 4649 but %s", p.Value)
+	}
+}
+
+func TestMap(t *testing.T) {
+	ch := make(chan PathValue)
+	go func() {
+		m := make(map[string]interface{})
+		m["hoge"] = "fuga"
+		Scan(m, ch)
+	}()
+
+	p := <-ch
+	if p.Path != "$.hoge" {
+		t.Fatalf("Path should be $.hoge but %s", p.Path)
+	}
+	if p.Value != "fuga" {
+		t.Fatalf("Value should be 'fuga' but %s", p.Value)
+	}
+}
