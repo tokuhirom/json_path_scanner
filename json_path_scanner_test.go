2036aab77fa2731a7ce2134c68cb9b2dcff63b60
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-23 09:54:34 +0900

    Fixes tests

--- json_path_scanner_test.go
+++ json_path_scanner_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestInt(t *testing.T) {
-	ch := make(chan PathValue)
+	ch := make(chan *PathValue)
 	go func() {
 		Scan(3, ch)
 	}()
@@ -19,12 +19,12 @@ func TestInt(t *testing.T) {
 		t.Fatalf("Path should be $ but %s", p.Path)
 	}
 	if p.Value != 3 {
-		t.Fatalf("Value should be 3 but %s", p.Value)
+		t.Fatalf("Value should be 3 but %v", p.Value)
 	}
 }
 
 func TestFloat(t *testing.T) {
-	ch := make(chan PathValue)
+	ch := make(chan *PathValue)
 	go func() {
 		Scan(3.14, ch)
 	}()
@@ -35,12 +35,12 @@ func TestFloat(t *testing.T) {
 		t.Fatalf("Path should be $ but %s", p.Path)
 	}
 	if p.Value != 3.14 {
-		t.Fatalf("Value should be 3.14 but %s", p.Value)
+		t.Fatalf("Value should be 3.14 but %v", p.Value)
 	}
 }
 
 func TestString(t *testing.T) {
-	ch := make(chan PathValue)
+	ch := make(chan *PathValue)
 	go func() {
 		Scan("Foo", ch)
 	}()
@@ -51,12 +51,12 @@ func TestString(t *testing.T) {
 		t.Fatalf("Path should be $ but %s", p.Path)
 	}
 	if p.Value != "Foo" {
-		t.Fatalf("Value should be 'Foo' but %s", p.Value)
+		t.Fatalf("Value should be 'Foo' but %v", p.Value)
 	}
 }
 
 func TestArray(t *testing.T) {
-	ch := make(chan PathValue)
+	ch := make(chan *PathValue)
 	go func() {
 		Scan([]interface{}{5963, 4649}, ch)
 	}()
@@ -67,7 +67,7 @@ func TestArray(t *testing.T) {
 		t.Fatalf("Path should be $[0] but %s", p.Path)
 	}
 	if p.Value != 5963 {
-		t.Fatalf("Value should be 5963 but %s", p.Value)
+		t.Fatalf("Value should be 5963 but %v", p.Value)
 	}
 
 	p = <-ch
@@ -75,12 +75,12 @@ func TestArray(t *testing.T) {
 		t.Fatalf("Path should be $[1] but %s", p.Path)
 	}
 	if p.Value != 4649 {
-		t.Fatalf("Value should be 4649 but %s", p.Value)
+		t.Fatalf("Value should be 4649 but %v", p.Value)
 	}
 }
 
 func TestMap(t *testing.T) {
-	ch := make(chan PathValue)
+	ch := make(chan *PathValue)
 	go func() {
 		m := make(map[string]interface{})
 		m["hoge"] = "fuga"
@@ -92,23 +92,23 @@ func TestMap(t *testing.T) {
 		t.Fatalf("Path should be $.hoge but %s", p.Path)
 	}
 	if p.Value != "fuga" {
-		t.Fatalf("Value should be 'fuga' but %s", p.Value)
+		t.Fatalf("Value should be 'fuga' but %v", p.Value)
 	}
 }
 
-func TestSynopsis(t *testing.T) {
-	ch := make(chan PathValue)
+func ExampleSynopsys() {
+	ch := make(chan *PathValue)
 	go func() {
 		var m interface{}
 		err := json.Unmarshal([]byte(`{
-			"hoge":"fuga",
-			"x":[
-				{
-					"y": 3,
-					"z": [1,2,3]
-				}
-			]
-		}`), &m)
+				"hoge":"fuga",
+				"x":[
+					{
+						"y": 3,
+						"z": [1,2,3]
+					}
+				]
+			}`), &m)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -116,6 +116,13 @@ func TestSynopsis(t *testing.T) {
 	}()
 
 	for p := range ch {
-		fmt.Printf("%s => %s\n", p.Path, p.Value)
+		fmt.Printf("%s => %v\n", p.Path, p.Value)
 	}
+
+	// Unordered  output:
+	// $.hoge => fuga
+	// $.x[0].y => 3
+	// $.x[0].z[0] => 1
+	// $.x[0].z[1] => 2
+	// $.x[0].z[2] => 3
 }
